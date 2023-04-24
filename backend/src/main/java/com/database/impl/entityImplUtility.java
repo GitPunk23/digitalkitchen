cec645432019fddea0c4adfab61e7f2b16889694
Brett Wuebben <NeonStar.Developer@Outlook.com>
2023-04-24 08:00:37 -0400

    feat: add entity utility static method

---
+++ backend/src/main/java/com/database/impl/entityImplUtility.java
@@ -0,0 +1,30 @@
+package com.database.impl;
+
+public class entityImplUtility {
+    
+    public static Entity resolveEntityType(String table, int ID) {
+        switch (table) {
+            case "categories":
+                return new CategoryImpl(ID);
+            case "ingredients":
+                return new IngredientsImpl(ID);
+            case "measurements":
+                return new MeasurementsImpl(ID);
+            case "recipe_ingredients":
+                return new RecipeIngredientsImpl(ID);
+            case "recipe_tags":
+                return new RecipeTagsImpl(ID);
+            case "recipes":
+                return new RecipesImpl(ID);
+            case "steps":
+                return new StepsImpl(ID);
+            case "tags":
+                return new TagsImpl(ID);
+        }
+
+
+    }
+
+
+
+}
