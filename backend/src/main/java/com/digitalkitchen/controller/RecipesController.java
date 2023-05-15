2d1c3af515c528b8c581f217e4683741965f2ec0
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-15 10:04:26 -0400

    feat: rest controller works!

--- backend/src/main/java/com/digitalkitchen/controller/RecipesController.java
+++ backend/src/main/java/com/digitalkitchen/controller/RecipesController.java
@@ -2,14 +2,24 @@ package com.digitalkitchen.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.digitalkitchen.service.RecipesService;
 
-@Controller
+@RestController
 @RequestMapping("/recipes")
 public class RecipesController {
 
     @Autowired
     private RecipesService recipesService;
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, World!";
+    }
 
 }
