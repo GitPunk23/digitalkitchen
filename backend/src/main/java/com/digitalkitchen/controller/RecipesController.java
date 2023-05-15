10bb0a3603f90282512b321722c0648a34f42345
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-15 13:04:30 -0400

    feat: add base post methods

--- backend/src/main/java/com/digitalkitchen/controller/RecipesController.java
+++ backend/src/main/java/com/digitalkitchen/controller/RecipesController.java
@@ -1,12 +1,16 @@
 package com.digitalkitchen.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.digitalkitchen.entities.Recipes;
 import com.digitalkitchen.service.RecipesService;
 
 @RestController
@@ -16,10 +20,25 @@ public class RecipesController {
     @Autowired
     private RecipesService recipesService;
 
-    @GetMapping("/")
-    @ResponseBody
-    public String home() {
-        return "Hello, World!";
+    @PostMapping("/addRecipe")
+    public ResponseEntity<?> createRecipe(@RequestBody Recipes recipe) {
+
+
+
+        return ResponseEntity.ok().build();
     }
 
+    @PostMapping("addSteps")
+    public ResponseEntity<?> addSteps() {
+
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("addIngredients")
+    public ResponseEntity<?> addIngredients() {
+
+
+        return ResponseEntity.ok().build();
+    }
 }
