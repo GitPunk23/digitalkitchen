ff196a5f053f8f69b6ae0e9a8d59bc5633991537
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-15 21:11:52 -0400

    feat: build router for forms

--- frontend/digital-kitchen/src/App.js
+++ frontend/digital-kitchen/src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeForm from './RecipeForm';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import IngredientForm from './IngredientForm';
 
 function WelcomePage() {
   return (
@@ -32,4 +12,18 @@ function WelcomePage() {
   );
 }
 
-export default WelcomePage;
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route exact path="/" element={<RecipeForm />} />
+          <Route path="/ingredients" element={<IngredientForm />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
