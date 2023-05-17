715f2528181501d70943c0fc0b8f6d5ee9f796f3
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-17 14:44:01 -0400

    feat: add deserializers

---
+++ backend/src/main/java/com/digitalkitchen/util/MeasurementsDeserializer.java
@@ -0,0 +1,24 @@
+package com.digitalkitchen.util;
+
+import java.io.IOException;
+
+import com.digitalkitchen.entities.Measurements;
+import com.digitalkitchen.service.MeasurementsService;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonComponent;
+
+@JsonComponent
+public class MeasurementsDeserializer extends JsonDeserializer<Measurements> {
+    @Autowired
+    private MeasurementsService measurementsService;
+
+    @Override
+    public Measurements deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        int measurementsId = Integer.parseInt(p.getValueAsString());
+        return measurementsService.getMeasurementById(measurementsId).orElse(null);
+    }
+}
+
