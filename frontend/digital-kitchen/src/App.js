dd7c93b929badd7b93c2a78d47d3937665429f81
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-18 09:53:28 -0400

    feat: add page routing to app

--- frontend/digital-kitchen/src/App.js
+++ frontend/digital-kitchen/src/App.js
@@ -1,14 +1,19 @@
+import HomePage from './components/HomePage';
+import CreateRecipePage from './components/CreateRecipePage';
 import MultiStepForm from './components/forms/MultiStepForm';
 import './styles/App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Create a new recipe</h1>
-      <MultiStepForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create-recipe" element={<CreateRecipePage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+//<MultiStepForm/>
