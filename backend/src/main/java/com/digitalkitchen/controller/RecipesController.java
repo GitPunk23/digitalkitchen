43d0cd011ae3dd5ade0436fb5cced145a80c47d0
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-18 07:08:23 -0400

    feat: get status of recipes endpoint

--- backend/src/main/java/com/digitalkitchen/controller/RecipesController.java
+++ backend/src/main/java/com/digitalkitchen/controller/RecipesController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,16 @@ public class RecipesController {
     private RecipesService recipesService;
     @Autowired
     private RecipesEndpointService endpointService;
+
+    //GET requests
+
+    @GetMapping("/status")
+    public ResponseEntity<?> status() {
+        //Return 202
+        return ResponseEntity.accepted().build();
+    }
+
+    //POST requests
 
     @PostMapping("/createRecipe")
     @CrossOrigin(origins = "http://localhost:3000")
