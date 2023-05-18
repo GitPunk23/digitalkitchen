68aad5faaa01bc2e9b2703ccad0cd017c42b93a9
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-18 13:33:58 -0400

    feat: steps form can be edited

--- frontend/digital-kitchen/src/components/forms/StepsForm.js
+++ frontend/digital-kitchen/src/components/forms/StepsForm.js
@@ -1,18 +1,51 @@
 import React, { useState } from "react";
 
 function StepsForm({ formData, setFormData, onPrevStep }) {
-    const [description, setDescription] = useState(formData && formData.steps ? '' : []);
+  const [description, setDescription] = useState("");
+  const [editingIndex, setEditingIndex] = useState(-1);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newStep = {
-      description,
-    };
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      steps: [...(prevFormData?.steps || []), newStep],
-    }));
+
+    if (editingIndex !== -1) {
+      // If editing an existing step
+      setFormData((prevFormData) => {
+        const updatedSteps = [...prevFormData.steps];
+        updatedSteps[editingIndex].description = description;
+        return {
+          ...prevFormData,
+          steps: updatedSteps,
+        };
+      });
+      setEditingIndex(-1);
+    } else {
+      // If adding a new step
+      const newStep = {
+        description,
+      };
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        steps: [...(prevFormData?.steps || []), newStep],
+      }));
+    }
     setDescription("");
+  };
+
+  const handleEditStep = (index) => {
+    const step = formData.steps[index];
+    setDescription(step.description);
+    setEditingIndex(index);
+  };
+
+  const handleDeleteStep = (index) => {
+    setFormData((prevFormData) => {
+      const updatedSteps = [...prevFormData.steps];
+      updatedSteps.splice(index, 1);
+      return {
+        ...prevFormData,
+        steps: updatedSteps,
+      };
+    });
   };
 
   return (
@@ -37,7 +70,9 @@ function StepsForm({ formData, setFormData, onPrevStep }) {
             </tr>
             <tr>
               <td>
-                <button type="submit">Add Step</button>
+                <button type="submit">
+                  {editingIndex !== -1 ? "Update Step" : "Add Step"}
+                </button>
               </td>
             </tr>
           </tbody>
@@ -48,7 +83,11 @@ function StepsForm({ formData, setFormData, onPrevStep }) {
         {formData && formData.steps && formData.steps.length > 0 ? (
           <ol>
             {formData.steps.map((step, index) => (
-              <li key={index}>{step.description}</li>
+              <li key={index}>
+                {step.description}
+                <button onClick={() => handleEditStep(index)}>Edit</button>
+                <button onClick={() => handleDeleteStep(index)}>Delete</button>
+              </li>
             ))}
           </ol>
         ) : (
