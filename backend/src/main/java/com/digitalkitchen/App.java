55ee27dbe6dfe703bd6ed38d8359eb1c7782ccf9
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-19 09:47:37 -0400

    refactor: clean unused variables

--- backend/src/main/java/com/digitalkitchen/App.java
+++ backend/src/main/java/com/digitalkitchen/App.java
@@ -1,11 +1,9 @@
 package com.digitalkitchen;
 
-import com.digitalkitchen.service.RecipesService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
@@ -17,10 +15,6 @@ import org.springframework.boot.SpringApplication;
 @ComponentScan("com.digitalkitchen.util")
 public class App implements CommandLineRunner
 {
-
-    @Autowired
-    RecipesService rs;
-
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
