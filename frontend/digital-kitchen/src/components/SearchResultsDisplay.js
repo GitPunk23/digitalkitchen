699c0076ce6788f9e70774af422148a2931a6aa1
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2023-05-28 14:18:05 -0400

    feat: can switch between recipes in display view

--- frontend/digital-kitchen/src/components/SearchResultsDisplay.js
+++ frontend/digital-kitchen/src/components/SearchResultsDisplay.js
@@ -1,12 +1,45 @@
-import DisplayPage from "./pages/DisplayPage";
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import DisplayPage from './pages/DisplayPage';
 
-const SearchResultsDisplay = ({ recipes, currentRecipe }) => {
+class SearchResultsDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentRecipeID: props.currentRecipeID,
+      recipes: props.recipes,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentRecipeID !== this.props.currentRecipeID) {
+      this.setState({ currentRecipeID: this.props.currentRecipeID });
+    }
+  }
+
+  handlePrevClick = () => {
+    this.setState((prevState) => ({
+      currentRecipeID: prevState.currentRecipeID - 1,
+    }));
+  };
+
+  handleNextClick = () => {
+    this.setState((prevState) => ({
+      currentRecipeID: prevState.currentRecipeID + 1,
+    }));
+  };
+
+  render() {
+    const { currentRecipeID, recipes } = this.state;
+
     return (
-        <div>
-            <DisplayPage data={currentRecipe}/>
-        </div>
+      <div key={currentRecipeID}>
+        <Button onClick={this.handlePrevClick}>{'<'}</Button>
+        <DisplayPage data={recipes[currentRecipeID]} />
+        <Button onClick={this.handleNextClick}>{'>'}</Button>
+      </div>
     );
-};
+  }
+}
 
 export default SearchResultsDisplay;
