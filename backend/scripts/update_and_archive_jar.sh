7a31721d93febc91ddd982250f067d0c62dfee14
Brett <NeonStar.Dev@Outlook.com>
2023-07-29 16:57:54 +0000

    build: jar files correctly deployed to container

--- backend/scripts/update_and_archive_jar.sh
+++ backend/scripts/update_and_archive_jar.sh
@@ -4,13 +4,20 @@
 cd ..
 
 # Move the current JAR to the archive directory
-mv *.jar archive/
+mv target/*.jar archive/
+
+# Rebuild
+mvn clean
+mvn package
 
 # Get the latest JAR filename
 LATEST_JAR=$(ls target/*.jar)
 
-# Move the latest JAR to the project's root
-mv "$LATEST_JAR" .
+# Copy the latest JAR to the project's root
+cp "$LATEST_JAR" app.jar
+
+# Remove temp file
+rm target/*.original
 
 # Print to console
 echo 'JAR updated and archived successfully'
