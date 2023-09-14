698685809368acf455c8f2e935f51e3baec143af
neonstar-dev <neonstar.developer@outlook.com>
2023-09-13 22:44:11 -0400

    create JUnit test files

---
+++ backend/src/test/java/service/CategoryServiceTest.java
@@ -0,0 +1,38 @@
+package com.digitalkitchen.service;
+import com.digitalkitchen.entities.Category;
+import com.digitalkitchen.repository.CategoryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+    
+    public Optional<Category> getCategoryById(int id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Optional<Category> getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void updateCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    public void deleteCategoryById(int id) {
+        categoryRepository.deleteById(id);
+    }
+}
---
+++ backend/src/test/java/service/StepsServiceTest.java
@@ -0,0 +1,5 @@
+@SpringBootTest
+public class StepsServiceTest {
+
+}
+
