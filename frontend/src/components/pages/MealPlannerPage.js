a20fdec8d229ce9bd01fb0eb0b540b610f8b2497
NeonStar-Dev <NeonStar.Developer@Outlook.com>
2024-01-16 20:50:34 -0500

    form data is saved for submission

--- frontend/src/components/pages/MealPlannerPage.js
+++ frontend/src/components/pages/MealPlannerPage.js
@@ -1,18 +1,42 @@
 import MealList from "../MealsList";
+import MealPlannerForm from "../MealPlannerForm";
 import FetchManager from "../util/FetchManager";
 import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
 
-useEffect(async () => {
-    const response = await FetchManager.fetchMeals();
-    console.log(response);
-});
+//useEffect(async () => {
+    //const response = await FetchManager.fetchMeals();
+    //console.log(response);
+//});
+
+const emptyMealPlan = {
+    date: '',
+    name: '',
+    type: '',
+    notes: '',	
+    tags: [],
+    recipes: []
+};
+
+const form = true;
 
 const MealPlannerPage = () => {
     return (
         <div>
             <h1>Meal Planner</h1>
+            { !form ? (
             <div>
-                <MealList ></MealList>
+                <MealList></MealList>
+            </div>
+             ) : (
+            <div>
+                <MealPlannerForm
+                    mealPlanFormat={emptyMealPlan}
+                ></MealPlannerForm>
+            </div>
+             )}
+            <div>
+                <Button>Plan a Meal</Button>
             </div>
         </div>
     );
