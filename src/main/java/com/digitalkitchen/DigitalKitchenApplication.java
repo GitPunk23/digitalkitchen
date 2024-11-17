2d8482443a0eda12d3916a481e0ed1079845728b
Brett <GitPunk23@outlook.com>
2024-11-17 17:49:52 -0600

    integrate dotenv and add properties files

--- src/main/java/com/digitalkitchen/DigitalKitchenApplication.java
+++ src/main/java/com/digitalkitchen/DigitalKitchenApplication.java
@@ -1,5 +1,6 @@
 package com.digitalkitchen;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,6 +13,8 @@ public class DigitalKitchenApplication implements CommandLineRunner
 {
     public static void main( String[] args )
     {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(DigitalKitchenApplication.class, args);
     }
 
