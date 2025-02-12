cdb17ca40b5f30dafe5e9f6ec694da25273a371f
Brett <GitPunk23@outlook.com>
2025-02-12 17:22:53 -0600

    Add names to category enums

--- src/main/java/com/digitalkitchen/recipes/enums/Category.java
+++ src/main/java/com/digitalkitchen/recipes/enums/Category.java
@@ -8,26 +8,32 @@ import java.util.List;
 @Getter
 public enum Category {
     
-    APPETIZER,
-    BEEF,
-    BREAD,
-    BREAKFAST,
-    DESSERT,
-    DRINK,
-    HOLIDAY,
-    PASTA,
-    PIZZA_AND_FLATBREADS,
-    PORK,
-    POULTRY,
-    SALAD,
-    SANDWICHES_AND_WRAPS,
-    SAUCE,
-    SAUSAGE,
-    SEAFOOD,
-    SIDE,
-    SOUP,
-    SPICES,
-    VEGETABLES;
+    APPETIZER("Appetizer"),
+    BEEF("Beef"),
+    BREAD("Bread"),
+    BREAKFAST("Breakfast"),
+    DESSERT("Dessert"),
+    DRINK("Drink"),
+    HOLIDAY("Holiday"),
+    PASTA("Pasta"),
+    PIZZA_AND_FLATBREADS("Pizza and Flatbreads"),
+    PORK("Pork"),
+    POULTRY("Poultry"),
+    SALAD("Salad"),
+    SANDWICHES_AND_WRAPS("Sandwiches and Wraps"),
+    SAUCE("Sauce"),
+    SAUSAGE("Sausage"),
+    SEAFOOD("Seafood"),
+    SIDE("Side"),
+    SOUP("Soup"),
+    SPICES("Spices"),
+    VEGETABLES("Vegetables");
+
+    public final String category;
+
+    Category(String category) {
+        this.category = category;
+    }
 
     public static List<String> getAllCategoryStrings() {
         return Arrays.stream(Category.values())
