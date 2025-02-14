857692ea9b6a1e28ef47f54d37234ef769ef95a4
Brett <GitPunk23@outlook.com>
2025-02-13 20:18:37 -0600

    this isn't a problem I want to solve right now actually

--- src/main/resources/db/migration/V02__ADD_MISSING_COLUMNS.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE recipes ADD COLUMN calories_per_serving INT;
-
-ALTER TABLE recipes ADD COLUMN description TEXT;
-
-ALTER TABLE recipes ADD COLUMN notes TEXT;
-
-ALTER TABLE recipes ADD COLUMN servings INT;
