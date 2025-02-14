237530bcb4bc966238d98fc55cb3dd8a63a0e055
Brett <GitPunk23@outlook.com>
2025-02-13 19:30:24 -0600

    fix actual db build

--- src/main/resources/db/migration/V02__ADD_MISSING_COLUMNS.sql
+++ src/main/resources/db/migration/V02__ADD_MISSING_COLUMNS.sql
@@ -1,7 +1,7 @@
-ALTER TABLE recipes ADD COLUMN IF NOT EXISTS calories_per_serving INT;
+ALTER TABLE recipes ADD COLUMN calories_per_serving INT;
 
-ALTER TABLE recipes ADD COLUMN IF NOT EXISTS description TEXT;
+ALTER TABLE recipes ADD COLUMN description TEXT;
 
-ALTER TABLE recipes ADD COLUMN IF NOT EXISTS notes TEXT;
+ALTER TABLE recipes ADD COLUMN notes TEXT;
 
-ALTER TABLE recipes ADD COLUMN IF NOT EXISTS servings INT;
+ALTER TABLE recipes ADD COLUMN servings INT;
